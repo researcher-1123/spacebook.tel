a46b52ed859a24de2586a99d396f1bc998db5390
pablopenrose <kaderarnold@icloud.com>
2021-09-17 01:33:53 -0700

    spacebook.tel waitlist..

---
+++ src/Waitlist.jsx
@@ -0,0 +1,12 @@
+class Waitlist {
+  render() {
+    <div id="waitlist-wrapper">
+	<input id="waitlist-input" />
+	<hr />
+	<button type="submit" id="enter">enter</buttton>
+    </div>
+  }
+  
+}
+
+export default Waitlist
